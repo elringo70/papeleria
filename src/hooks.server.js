87cf96ebccdafb6284f20c8581eff10a594e46ed
elringo70 <elringo70@hotmail.com>
2023-04-18 22:33:59 -0600

    progress

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,33 @@
-/* import { dbConnect, dbDisconnect } from '$utils/db';
+import { redirect } from '@sveltejs/kit';
+import admin from './utils/firebase-admin';
 
-const dbConnection = async () => {
-	await dbConnect();
+const publicRoutes = ['/', '/register', '/login'];
+
+export const handle = async ({ event, resolve }) => {
+	const token = event.cookies.get('session');
+	const currentLocation = event.url.pathname;
+
+	let decodedToken = null;
+	try {
+		decodedToken = await admin.auth().verifyIdToken(token);
+
+		const user = {
+			name: decodedToken.name,
+			email: decodedToken.email,
+			picture: decodedToken.picture,
+			uid: decodedToken.uid
+		};
+
+		event.locals.user = user;
+	} catch (error) {
+		event.locals.user = null;
+		event.cookies.delete('session');
+
+		if (!publicRoutes.includes(currentLocation)) throw redirect(303, '/');
+	}
+
+	if (decodedToken && publicRoutes.includes(currentLocation)) throw redirect(301, '/profile');
+
+	const response = await resolve(event);
+	return response;
 };
-
-dbConnection();
- */
