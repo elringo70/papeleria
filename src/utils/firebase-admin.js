a584a45abfc6518722a62f197e3b041ad38f2d3f
elringo70 <elringo70@hotmail.com>
2023-04-18 22:58:55 -0600

    progress

--- src/utils/firebase-admin.js
+++ src/utils/firebase-admin.js
@@ -1,8 +1,11 @@
 import admin from 'firebase-admin';
-const serviceAccount = require('../../firebase-admin.json');
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
-});
+import serviceAccount from 'C:/Users/elrin/Documents/GitHub/papeleria/firebase-admin.json';
+
+if (!admin.apps.length) {
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount)
+	});
+}
 
 export default admin;
