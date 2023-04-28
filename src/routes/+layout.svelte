91c35f616c21675b891b6d077b7363d168b4f5cb
elringo70 <elringo70@hotmail.com>
2023-04-28 01:54:40 -0600

    progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script>
 	export let data;
-	export let logoutFromGoogle;
 
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<Navbar user={data.user} {logoutFromGoogle} />
+<Navbar user={data?.user} />
 
 <slot />
